5847b80f46a347ff81eebe15f453c3872d98de2c
MrBeelo <mrbeelomakris@gmail.com>
2026-07-07 21:36:44 +0300

    added the holy readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Hexamania.io
+
+Hexamania description goes here
+
+## IDEAS
+
+#### DAY 1
+- ~~Start out in the center of a map, nothing else around you~~
+- ~~Can move with arrow keys, physics like Epic Asteroid Game (just steal em lol)~~
+- Shoot out little pellets/circles as the base attack
+- Evil hexagons, developed as the player, try to attack you
+- Find hexagons (like normal little ones) by killing enemy hexagons (like the developed ones, like you!), go onto them to M E R G E
+- Merging combines that hexagon with yours, making you bigger and more resilient
+- Hexagons have different types (with different colors)! Joining them buffs a certain stat.
+- ~~Get to 66 hexagons (should probably change to a number so that the player is a perfect hexagon) stuck together to REACH MAX!~~
+- eating hexagons after being max will give you points or smth
+- a ye add points
+
+#### DAY 2
+- Enemies get bigger and stronger as time passes
+- Powerup hexagons: spawn randomly around the map, give either a temporary buff or a health boost idk
+- Oh ye health, self explanatory (should be at the bottom middle of the screen lol)
+- Grade when you die XD
+- SPELLSSSSS
+
+#### DAY 3
+- Hexagon has eyes <3 that look where your mouse is!
+- Background should have falling hexagons lol
+- Blur/bloom, lots of blur/bloom
+- Design a couple different hexagon types
+
+#### DAY 4
+- Sound effects
+- Music should be 6/4 lol, calm and relaxing with reverb/echo
--- bin/web/index.js
+++ bin/web/index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp766dm5iv.js
+// include: /tmp/tmpbruirio7.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -199,21 +199,21 @@ Module['FS_createPath']("/", "res", true, true);
 
   })();
 
-// end include: /tmp/tmp766dm5iv.js
-// include: /tmp/tmpwa9s_hfe.js
+// end include: /tmp/tmpbruirio7.js
+// include: /tmp/tmpr5bg3cvj.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpwa9s_hfe.js
-// include: /tmp/tmp3rhn19q3.js
+  // end include: /tmp/tmpr5bg3cvj.js
+// include: /tmp/tmpv9zgeukd.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp3rhn19q3.js
+  // end include: /tmp/tmpv9zgeukd.js
 
 
 var programArgs = [];
